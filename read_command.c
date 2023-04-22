c8e6f7f5160a71e231abc25247ebc0ccfb1cb033
Elisha <elishae621@gmail.com>
2023-04-23 00:11:14 +0100

    impliment EOF

--- read_command.c
+++ read_command.c
@@ -8,11 +8,19 @@ void read_command(char *par[])
 {
 	char *line = NULL;
 	size_t linecap = 0;
-	int i = 0, j = 0, len;
+	int i = 0, j = 0, len, rdlen;
 	char *array[100], *pch, *delim = " ";
 
 	/* Read one line */
-	getline(&line, &linecap, stdin);
+	rdlen = getline(&line, &linecap, stdin);
+
+	/* check for EOF */
+	if (rdlen == -1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "\n", 2);
+		exit(0);
+	}
 
 	pch = strtok(line, delim);
 
