4ea8ec70081952807e5a027e8d87d14f4a30f2bb
Uchechukwu <uchesy4u@yahoo.com>
2023-04-26 05:08:56 -0700

    type_prompt.c updated

--- type_prompt.c
+++ type_prompt.c
@@ -1,45 +1,74 @@
 #include "shell.h"
 
-#define BUFFER_SIZE 1024
+/**
+ * check_interactive - checks if the shell is in interactive mode
+ * @shell_info: a pointer to a struct holding information about the shell
+ *
+ * Return: 1 if the shell is in interactive mode, 0 otherwise
+ */
+int check_interactive(struct shell_info *shell_info)
+{
+	return (isatty(STDIN_FILENO) && shell_info->read_fd <= 2);
+}
 
 /**
- * main - Entry point for the program
- * Description: This program implements a simple UNIX shell that
- * displays a prompt, reads a line of input from stdin, and repeats
- * the process until the user enters an EOF character (Ctrl+D).
+ * is_delimiter - checks if a character is a delimiter
+ * @c: the character to check
+ * @delimiter: a string containing the delimiters to check against
  *
- * Return: Always 0
+ * Return: 1 if the character is a delimiter, 0 otherwise
  */
-
-int main(void)
+int is_delimiter(char c, char *delimiter)
 {
-	char buffer[BUFFER_SIZE];
-	char *args[2];
-	int status;
-
-	while (1)
+	while (*delimiter)
 	{
-		printf("$ ");
-		if (!fgets(buffer, BUFFER_SIZE, stdin))
-		{
-			break;
-		}
-		buffer[strcspn(buffer, "\n")] = '\0';
-		args[0] = buffer;
-		args[1] = NULL;
-
-		if (fork() == 0)
-		{
-			if (execve(args[0], args, NULL) == -1)
-			{
-				printf("Error: command not found\n");
-			}
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			wait(&status);
-		}
+		if (*delimiter++ == c)
+			return (1);
 	}
-	return (EXIT_SUCCESS);
+	return (0);
+}
+
+/**
+ * is_alphabetic - checks if a character is an alphabetic character
+ * @c: the character to check
+ *
+ * Return: 1 if the character is an alphabetic character, 0 otherwise
+ */
+int is_alphabetic(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * string_to_integer - converts a string to an integer
+ * @s: the string to convert
+ *
+ * Return: the integer value of the string, or 0 if no numbers are present
+ */
+int string_to_integer(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+	return (output);
 }
